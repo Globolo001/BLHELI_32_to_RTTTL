45bf2bd6428213c681bdc9e56c12f5ba81db8c96
Globolousr/local/bin/python3 /Users/oskar/Develop/ForRealFPV/BLHELI_32_to_RTTTL/main.py <globolo@gmx.net>
2022-07-11 14:20:23 +0200

    Added 1/x formatting and now checks for correct notes via regex

--- main.py
+++ main.py
@@ -8,6 +8,46 @@
 
 #### Just some code #########################################################################################################################
 
+import re
+
+accepted_notation_regex = '^[CDEFGABP]#?\d{0,4}$|^\d[\/]\d{1,2}$|^\d$'
+
+#For BLHELI_32 notation, with spaces between notes and duration ("A#5 8 P8 A#5 8"), apply this function to get formatted ("A#58 P8 A#58")
+def get_into_right_format(string):
+    working_string = string.split(" ")
+
+    #do basic preperation of the string
+    working_string = [x for x in working_string if x != ""]
+    working_string = [x for x in working_string if x != "\t"]
+    working_string = [x for x in working_string if x != "\n"]
+
+    #remove quotation marks
+    working_string = [x.replace("\"","") for x in working_string]
+
+    #go through each note
+    for i in range(len(working_string)):
+        if(i>=len(working_string)):
+            break
+        if len(working_string[i]) == 1 and working_string[i].isdigit():
+            #if it is a duration as a single digit, append to the previous note if it exists
+            if len(working_string)>1:
+                working_string[i-1] += working_string[i]
+                working_string.pop(i)
+            else:
+                print("ERROR: Should not start with a number: " + working_string[i])
+                working_string.pop(i)
+        elif "/" in working_string[i] and len(working_string[i]) >= 3 and working_string[i][2].isdigit():
+            #if it is a duration in the form of "1/x", append to the previous note
+            #print("Test: " + working_string[i])
+            beat = working_string[i].split("/")
+            working_string[i-1] += beat[1]
+            working_string.pop(i)
+        #match regex of note and duration
+        elif not re.match(accepted_notation_regex, working_string[i]):
+            print("ERROR Unknown Symbol: '" + working_string[i] + "'")
+            working_string.pop(i)
+    return " ".join(working_string)
+
 #split each note into note and duration
 def split_into_note_and_duration(input):
     #if it contains #, it is a sharp note. get the position of the #
@@ -62,31 +102,36 @@ def convert_unformatted_string_to_rttl(prefix,source_Melody):
 def insert_number(string, number):
     return string[:string.index(":")] + str(number) + string[string.index(":"):]
 
-#For BLHELI_32 notation, with spaces between notes and duration ("A#5 8 P8 A#5 8"), apply this function to get formatted ("A#58 P8 A#58")
-def get_into_right_format(string):
-    working_string = string.split(" ")
-    #go through each note. if it has only one character, append to the previous note
-    for i in range(len(working_string)):
-        if(i==len(working_string)):
-            break
-        if len(working_string[i]) == 1:
-            working_string[i-1] += working_string[i]
-            working_string.pop(i)
-        if(i==len(working_string)):
-            break
-    return " ".join(working_string)
+
+################################# Testing Ground ##############################################
+
+song1 = "D5 8 E5 8 G5 8 D5 8 B5 4 P8 B5 4 P8 A5 2 P4 D5 8 E5 8 G5 8 D5 8 A5 4 P8 A5 4 P8 G5 4 G5 8 F#5 8 E5 4 D5 8 E5 8 G5 8 E5 8 G5 2 A5 4 F#5 4 F#5 8 E5 8 D5 4 D5 8 P 8 D5 8 P8 A5 4 A5 8 P8 G5 2"
+song2 = "C64 A#54 A54 F54 G52 G54 D64 C62 A#52 A52 A54 A54 C62 A#54 A54 G52 G54 A#64 A64 A#64 A64 A#64 G52 G54 A#64 A64 A#64 A64 A#64"
+song3 = "E6 1/4  D6 1/4  F#5 1/2  G#5 1/2    C#6 1/4    B5 1/4    D5 1/2     E5 1/2  B5 1/4  A5 1/4  C#5 1/2 E5 1/2 A5 1/1"
+song4_broken = "D5 8 E5 8 G3 2 F#5 1/ J#6 1/4 B5 14"
+
+print (convert_unformatted_string_to_rttl("testing1", song1))
+print (convert_unformatted_string_to_rttl("testing2", song2))
+print (convert_unformatted_string_to_rttl("testing3", song3))
+print (convert_unformatted_string_to_rttl("testing4", song4_broken))
+
 
 ################################# COMMANDLINE UI ##############################################
 #make a command-line interface
 #get the input
 print("\n\n##### WELCOME TO THE BLHELI_32 to BLUE JAY MUSIC CONVERTER #####\n\n")
 
+name_prompt = "Enter your melody name (default/blank is \"test\"):"
+speed_prompt = "Enter your speed (default/blank is 210): "
+output_melody_formatting = "Melody fomatting is: \n\"A#58 P8 G516\" OR\n\"A#5 8 P4 G5 16\" OR\n\"A#5 1/8 P 1/8 G5 1/16\" OR mixed"
+input_prompt = "Enter your melody for ESC{}: (type \"exit\" to exit)"
+
 while True:
-    name = input("Enter your melody name (default is \"test\"):")
+    name = input(name_prompt)
     if name == "":
         name = "test"
     #enter speed or leave blank for default
-    speed = input("Enter your speed (default is 210): ")
+    speed = input(speed_prompt)
     #if speed is not a number an integer, set it to 210
     if speed == "" or not speed.isdigit():
         speed = 210
@@ -94,10 +139,10 @@ while True:
         speed = int(speed)
     header = name + ":b=" + str(speed) + ",o=3,d=4"
     print("Header is \"" + header + "\"")
-    print("Melody fomatting is: \"A#5 8 P8 A#5 8\" OR \"A#58 P8 A#58\" OR mixed")
+    print(output_melody_formatting)
     melodies = []
     for i in range(4):
-        input_melody = input("Enter your melody for ESC{}: (type \"exit\" to exit)".format(i+1))
+        input_melody = input(input_prompt.format(i+1))
         if input_melody == "exit":
             break
         #convert the input to rttl and save to list
