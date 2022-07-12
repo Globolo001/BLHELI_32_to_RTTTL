db84a1fc3bf4dea2f10a5f04832f72a68800c8a6
Globolousr/local/bin/python3 /Users/oskar/Develop/ForRealFPV/BLHELI_32_to_RTTTL/main.py <58426549+Globolo001@users.noreply.github.com>
2022-07-12 11:50:05 +0200

    Now returns string and warnings as small array

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 // Alpha in JavaScript
 
-// This program takes a BLHELI_32 music string and converts it to RTTTL for BlueJay ESC
-
 /** 
- * API: Call 'convert_unformatted_string_to_rttl' with a prefix and an unformatted BL_HELI_32 string;
- * Prefix systax: '${name}:d=8,o=5,b=${speed_int}:'
- * Or call convert_unformatted_string_to_rttl_stock_prefx with an unformatted BL_HELI_32 string;
+ * This program takes a BLHELI_32 music string and converts it to RTTTL for BlueJay ESC.
+ * 
+ * API: Call 'convert_unformatted_string_to_rttl_with_invalid_symbols' with a prefix and an unformatted BL_HELI_32 string;
+ * It returns the rtttl string and the invalid-symbols-strings-array (eg [32DJ, 1/]) as an array;
+ * 'convert_unformatted_string_to_rttl' only returns the rtttl string without the invalid-symbols-strings;
+ * 
+ * Prefix systax: '${name}:d=8,o=5,b=${speed_int}'; (No colon ':').
+ * To not use a prefix, call 'convert_unformatted_string_to_rttl_stock_prefix' with an unformatted BL_HELI_32 string and no prefix;
  */
+
+// Change if you want a commandline interface (using JavaScript's prompt() function as input)
+const command_line_ui_active = false;
 
 // ############################# Just some code #############################
 
 const accepted_notation_regex = new RegExp('^[CDEFGABP]#?[0-9]{0,4}$|^[0-9][/\\/][0-9]{1,2}$|^[0-9]$');
-const replacesymbols_regex = new RegExp("\"|\'", 'g');
+const replacesymbols_regex = new RegExp("\"|\'|\n", 'g');
 const remove_regex = new RegExp("\s|\t|\n", 'g');
 const stock_rtttl_prefix = 'test:d=8,o=5,b=210:';
 
@@ -23,25 +29,27 @@ const stock_rtttl_prefix = 'test:d=8,o=5,b=210:';
 function get_into_right_format(input_string) {
     input_string = input_string.replaceAll(replacesymbols_regex, '');
     working_array = input_string.split(' ');
-    
+
+    invalid_notes = [];
+
     // Filter for only valid symbols
-    better_array = working_array.filter(function(element) {
-        if(remove_regex.test(element) || element == '') {
+    better_array = working_array.filter(function (element) {
+        if (remove_regex.test(element) || element == '') {
             return false;
-        }else if (accepted_notation_regex.test(element)) {
+        } else if (accepted_notation_regex.test(element)) {
             return true;
         } else {
-            console.log('ERROR: Invalid note: ' + element);
+            invalid_notes.push(element);
             return false;
         }
     });
 
-    // Bring into correct notation: eg 'E#5 1/4" -> E#54
+    // Bring into correct notation: eg 'E#5 1/4' -> 'E#54'
     correct_string_array = [];
     correct_string_array.push(better_array[0]); //Always add the first element
     better_array.shift(); //Remove the first element
 
-    better_array.forEach(function(element) {
+    better_array.forEach(function (element) {
         if (!isNaN(element)) {
             // If it is a duration in format 'X' (eg 4): add it to the last note
             correct_string_array[correct_string_array.length - 1] += element;
@@ -52,25 +60,30 @@ function get_into_right_format(input_string) {
             correct_string_array.push(element);
         }
     });
-    return correct_string_array.join(' ');
+    return [correct_string_array.join(' '), invalid_notes];
 }
 
 /**
  * A simple class for a note.
  */
 class Note {
+    /**
+     * The constructor for a note.
+     * @param {String} tune The tune of the note (eg. 'C#5')
+     * @param {String} duration The duration of the note (eg. '4')
+     */
     constructor(tune, duration) {
-      this.tune = tune;
-      this.duration = duration;
+        this.tune = tune;
+        this.duration = duration;
     }
     get_rtttl_string() {
         var rtttl = this.duration + this.tune.toLowerCase();
         return rtttl;
-  }
+    }
 }
 
 /**
- * Split a note and duration into a Note object from a formatted String
+ * Split a note and duration into a Note object from a formatted String.
  * @param {String} note_string Formatted string of a single note with a duration (eg 'E#54')
  * @returns {Note} Note object;
  */
@@ -78,59 +91,72 @@ function split_into_note_object(note_string) {
     if (note_string.includes('#')) {
         // Sharp note
         position = note_string.indexOf('#');
-        note = note_string.substring(0, position+2);
-        duration = note_string.substring(position+2);
+        note = note_string.substring(0, position + 2);
+        duration = note_string.substring(position + 2);
         return new Note(note, duration);
     } else if (note_string.includes('P')) {
         // Pause
         return new Note('p', note_string.substring(1));
     } else if (note_string.length >= 2) {
         // Regular note
-        return new Note(note_string.substring(0,2), note_string.substring(2));
+        return new Note(note_string.substring(0, 2), note_string.substring(2));
     } else {
         return;
     }
 
 }
 /**
- * Go through each note in the String and return as list of notes as Note objects;
- * @param {String} source_Melody
+ * Go through each note in the String and return as list of notes as Note objects.
+ * @param {String} source_Melody A formatted BLHELI_32 string
  * @returns {Array<Note>} Array of Note objects;
  */
 function split_all_notes(source_Melody) {
     note_string = source_Melody.split(' ');
     notes = [];
-    note_string.forEach(function(note) {
+    note_string.forEach(function (note) {
         note = split_into_note_object(note)
         if (note != undefined) {
             notes.push(note);
         }
     });
     return notes;
-    
+
 }
 
 /**
- * Converts an Arrray of Note objects to an rtttl string;
- * @param {Array<Note>} notes 
+ * Converts an Arrray of Note objects to an rtttl string.
+ * @param {Array<Note>} notes Array of Note objects
  * @returns {String} Rtttl String;
  */
 function compile_notes_to_rtttl(notes) {
-    result_list =  notes.map(function(note) {
+    result_list = notes.map(function (note) {
         return note.get_rtttl_string();
     });
     return result_list.join(',');
 }
 
 /**
- * Converts a unformatted BLHELI_32 string to rtttl string;
+ * Converts a unformatted BLHELI_32 string to rtttl string; Returns the rtttl string and the invalid symbols.
+ * @param {String} prefix Prefix for the rtttl string without colon ':'
+ * @param {String} unformatted_source_Melody Unformatted string of a melody in any BLHELI_32 notation
+ * @returns {Array<String,Array<String>} Compiled rtttl string and an array of invalid symbols;
+ */
+function convert_unformatted_string_to_rttl_with_invalid_symbols(prefix, unformatted_source_Melody) {
+    right_format_with_warnings = get_into_right_format(unformatted_source_Melody);
+    formatted_string = right_format_with_warnings[0];
+    invalid_symbols = right_format_with_warnings[1];
+    notes = split_all_notes(formatted_string);
+    return [prefix + ':' + compile_notes_to_rtttl(notes), invalid_symbols];
+}
+
+/**
+ * Converts a unformatted BLHELI_32 string to rtttl string; Returns the rtttl string.
  * @param {String} prefix Prefix for the rtttl string without colon ':'
  * @param {String} unformatted_source_Melody Unformatted string of a melody in any BLHELI_32 notation
  * @returns {String} Compiled rtttl string;
  */
-function convert_unformatted_string_to_rttl(prefix,unformatted_source_Melody) {
-    notes = split_all_notes(get_into_right_format(unformatted_source_Melody));
-    return prefix + ':' + compile_notes_to_rtttl(notes);
+function convert_unformatted_string_to_rttl(prefix, unformatted_source_Melody) {
+    return convert_unformatted_string_to_rttl_with_invalid_symbols(prefix, unformatted_source_Melody)[0];
 }
 
 /**
@@ -138,8 +164,8 @@ function convert_unformatted_string_to_rttl(prefix,unformatted_source_Melody) {
  * @param {String} nformatted_source_Melody Unformatted string of a melody in any BLHELI_32 notation
  * @returns {String} Compiled rtttl string;
  */
-function convert_unformatted_string_to_rttl_stock_prefx(unformatted_source_Melody) {
-    convert_unformatted_string_to_rttl(stock_rtttl_prefix, unformatted_source_Melody);
+function convert_unformatted_string_to_rttl_stock_prefix(unformatted_source_Melody) {
+    convert_unformatted_string_to_rttl_with_invalid_symbols(stock_rtttl_prefix, unformatted_source_Melody);
 }
 
 /**
@@ -171,56 +197,60 @@ console.log(convert_unformatted_string_to_rttl("testing5", song5_broken)) */
 
 // ############################# COMMANDLINE UI #############################
 
-song_name_prompt = 'Enter your melody name (default/blank is \"test\"):';
-speed_prompt = 'Enter your speed (default/blank is 210): ';
-output_melody_formatting = 'Melody fomatting is: \n\"A#58 P8 G516\" OR\n\"A#5 8 P4 G5 16\" OR\n\"A#5 1/8 P 1/8 G5 1/16\" OR mixed\nAdditional Spaces are removed';
+if (command_line_ui_active) {
+    song_name_prompt = 'Enter your melody name (default/blank is \"test\"):';
+    speed_prompt = 'Enter your speed (default/blank is 210): ';
+    output_melody_formatting = 'Melody fomatting is: \n\"A#58 P8 G516\" OR\n\"A#5 8 P4 G5 16\" OR\n\"A#5 1/8 P 1/8 G5 1/16\" OR mixed\nAdditional Spaces are removed';
 
-while (true) {
-    var song_name = prompt(song_name_prompt);
-    if (song_name == null) {
-        break;
-    }
-    if (song_name == '') {
-        song_name = 'test';
-    }
-    speed = prompt(speed_prompt);
-    if (speed == null) {
-        break;
-    }
-    if (speed == '' || isNaN(speed)) {
-        speed = "210";
-    }
-    header = song_name + ':b=' + speed + ',o=3,d=4';
-    console.log('Header is \"' + header + '\"');
-    console.log(output_melody_formatting);
-    
-    // repeat four times
-        
-    melodies = [];
-    for (i = 0; i < 4; i++) {
-        input_melody = prompt(`Enter your melody for ESC${i+1}: (type \"exit\" to exit)`);
-        if (input_melody == 'exit' || input_melody == null) {
+    while (true) {
+        var song_name = prompt(song_name_prompt);
+        if (song_name == null) {
             break;
         }
-        melodies.push(convert_unformatted_string_to_rttl(insert_number(header,i+1),input_melody));
-        console.log(`\nESC${i+1}: ${melodies[i]}\n`);
-    }
+        if (song_name == '') {
+            song_name = 'test';
+        }
+        speed = prompt(speed_prompt);
+        if (speed == null) {
+            break;
+        }
+        if (speed == '' || isNaN(speed)) {
+            speed = "210";
+        }
+        header = song_name + ':b=' + speed + ',o=3,d=4';
+        console.log('Header is \"' + header + '\"');
+        console.log(output_melody_formatting);
 
-    console.log('\n\n#########  ALL MELODIES #########\n');
-   
-    // print all melodies in the array in following lines
-    for (i = 0; i < melodies.length; i++) {
-        console.log(melodies[i]);
-    }
-    
-    cont = prompt('\nDo you want to continue? (y/n)');
-    if (cont == 'n' || cont == null) {
-        break;
-    } else if (cont == 'y') {
-        console.log('\n\n');
-        continue;
-    } else {
-        console.log('Invalid input');
-        break;
+        // repeat four times for four ESCs
+
+        melodies = [];
+        for (i = 0; i < 4; i++) {
+            input_melody = prompt(`Enter your melody for ESC${i + 1}: (type \"exit\" to exit)`);
+            if (input_melody == 'exit' || input_melody == null) {
+                break;
+            }
+            melody_and_warning = convert_unformatted_string_to_rttl_with_invalid_symbols(insert_number(header, i + 1), input_melody);
+            melodies.push(melody_and_warning[0]);
+            console.log(`Invalid symbols: ${melody_and_warning[1].join(', ')}`);
+            console.log(`\nESC${i + 1}: ${melodies[i]}\n`);
+        }
+
+        console.log('\n\n#########  ALL MELODIES #########\n');
+
+        // print all melodies in the array in following lines
+        for (i = 0; i < melodies.length; i++) {
+            console.log(melodies[i]);
+        }
+
+        cont = prompt('\nDo you want to continue? (y/n)');
+        if (cont == 'n' || cont == null) {
+            break;
+        } else if (cont == 'y') {
+            console.log('\n\n');
+            continue;
+        } else {
+            console.log('Invalid input');
+            break;
+        }
     }
 }
